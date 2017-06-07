9f5ada5cd1eba7c3aeed8e1ee87d5ac1bfa6538c
houdunwang <2300071698@qq.com>
2017-06-07 17:42:41 +0800

    no message

--- src/helper.php
+++ src/helper.php
@@ -1,13 +1,53 @@
 <?php
-/**
- * 请求参数
- *
- * @param        $var     变量名
- * @param null   $default 默认值
- * @param string $filter  数据处理函数
- *
- * @return mixed
- */
+if ( ! function_exists('request_url')) {
+    /**
+     * 请求的URL地址
+     *
+     * @return string
+     */
+    function request_url()
+    {
+        return trim('http://'.$_SERVER['HTTP_HOST'].'/'.trim($_SERVER['REQUEST_URI'], '/\\'), '/');
+    }
+}
+
+if ( ! function_exists('history_url')) {
+    /**
+     * 来源链接
+     *
+     * @return string
+     */
+    function history_url()
+    {
+        return isset($_SERVER["HTTP_REFERER"]) ? $_SERVER["HTTP_REFERER"] : '';
+    }
+}
+if ( ! function_exists('root_url')) {
+    /**
+     * 网站根地址URI
+     *
+     * @return string
+     */
+    function root_url()
+    {
+        return trim('http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']), '/\\');
+    }
+}
+
+if ( ! function_exists('web_url')) {
+    /**
+     * 网站主页
+     * 根据配置项 http.rewrite判断
+     * 没有开启伪静态时添加index.php入口文件
+     *
+     * @return string
+     */
+    function web_url()
+    {
+        return Config::get('http.rewrite') ? root_url() : root_url().'/index.php';
+    }
+}
+
 if ( ! function_exists('q')) {
     /**
      * 取得或设置全局数据包括:
